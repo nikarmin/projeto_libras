70a45935208c139196ff8e9659d91581705fb8d1
Nicoli <nikafworks@gmail.com>
2021-09-10 15:02:28 -0300

    Alteração

--- projeto-libras/scriptHome.js
+++ projeto-libras/scriptHome.js
@@ -1,3 +1,15 @@
-    function login(){
-        window.open('login.html', '_blank');
+/*Arrumar o JavaScript*/ 
+
+const botao = document.getElementById("cancelBtn");
+const painel = document.getElementById('painelLogin');
+
+/*botao.addEventListener("click", ()=> {
+    painel.style.animation = "";
+    setTimeout(() => painel.style.animation = "animatezoom 1s", 5);
+});
+
+window.onclick = function(event) {
+    if (event.target == painel) {
+        painel.style.display = "none";
     }
+}*/
