0e29487e7ac4981d190caf1510e0659262ebe832
Nicoli <nikafworks@gmail.com>
2021-09-22 09:13:53 -0300

    Arrumando o Slide

--- projeto-libras/index.js
+++ projeto-libras/index.js
@@ -5,3 +5,47 @@ function btnLogin(a)
     console.log(a);
     document.getElementById('painelLogin').style.display='block';
 } 
+
+/*SlideShow Icarly*/
+/*Arrumar o Icarly :(*/
+
+const slider = document.querySelector(".items");
+const slides = document.querySelectorAll(".item");
+const button = document.querySelectorAll(".button");
+
+
+let current = 0;
+let prev = 25;
+let next = 26;
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+}
+
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+const gotoNext = () => current < 25 ? gotoNum(current + 1) : gotoNum(0);
+
+const gotoNum = number => {
+    current = number;
+    prev = current - 1;
+    next = current + 1;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+        slides[i].classList.remove("prev");
+        slides[i].classList.remove("next");
+    }
+
+    if (next == 26) {
+        next = 0;
+    }
+
+    if (prev == -1) {
+        prev = 25;
+    }
+
+    slides[current].classList.add("active");
+    slides[prev].classList.add("prev");
+    slides[next].classList.add("next");
+}
