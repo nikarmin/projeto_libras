9bf07d2346d7efaeaf4a7891fb327505091b8bde
Nicoli <nikafworks@gmail.com>
2021-09-22 14:54:51 -0300

    Arrumando SlideShow

--- projeto-libras/index.js
+++ projeto-libras/index.js
@@ -7,45 +7,30 @@ function btnLogin(a)
 } 
 
 /*SlideShow Icarly*/
-/*Arrumar o Icarly :(*/
+/*Icarly ARRUMADO, falta uns ajustes*/
 
-const slider = document.querySelector(".items");
-const slides = document.querySelectorAll(".item");
-const button = document.querySelectorAll(".button");
+var slideIndex = 1;
+showSlides(slideIndex);
 
-
-let current = 0;
-let prev = 25;
-let next = 26;
-
-for (let i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+function plusSlides(n){
+    showSlides(slideIndex += n);
 }
 
-const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-const gotoNext = () => current < 25 ? gotoNum(current + 1) : gotoNum(0);
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
 
-const gotoNum = number => {
-    current = number;
-    prev = current - 1;
-    next = current + 1;
+    if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
 
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("active");
-        slides[i].classList.remove("prev");
-        slides[i].classList.remove("next");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
     }
 
-    if (next == 26) {
-        next = 0;
-    }
-
-    if (prev == -1) {
-        prev = 25;
-    }
-
-    slides[current].classList.add("active");
-    slides[prev].classList.add("prev");
-    slides[next].classList.add("next");
+    /*erro do que senhor amado*/
+    slides[slideIndex - 1].style.display = 'block';
 }
