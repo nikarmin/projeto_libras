bdff75fa94bda77354de46fe7babbfdfc4e44c09
Nicoli <nikafworks@gmail.com>
2021-09-24 13:47:33 -0300

    Arrumando o SlideShow

--- projeto-libras/html/index.js
+++ projeto-libras/html/index.js
@@ -6,9 +6,7 @@ function btnLogin(a)
     document.getElementById('painelLogin').style.display='block';
 } 
 
-/*SlideShow Icarly*/
-/*Icarly ARRUMADO, ARRUMADO II,
-wtf clique n funciona*/
+/*SlideShow*/
 
 var slideIndex = 0;
 showSlides(slideIndex);
@@ -17,21 +15,16 @@ function plusSlides(n){
     showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
 function showSlides(n){
     var i;
     var slides = document.getElementsByClassName("mySlides");
 
     if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
+    if (n < 0) {slideIndex = slides.length}
 
-    for (i = 0; i < slides.length; i++) {
+    for (i = 1; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
 
-    /*erro do que senhor amado*/
     slides[slideIndex - 1].style.display = 'block';
 }
