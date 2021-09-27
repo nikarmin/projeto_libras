167eced5043a7bc2b356d734756d03cbf5b024ca
Nicoli <nikafworks@gmail.com>
2021-09-27 11:58:18 -0300

    Alterando sugestionBox/commmentBox

--- projeto-libras/html/index.js
+++ projeto-libras/html/index.js
@@ -28,3 +28,98 @@ function showSlides(n){
 
     slides[slideIndex - 1].style.display = 'block';
 }
+
+/*Sistema de avaliação por comentário/estrela*/
+
+function avaliar(estrela) {
+    var url = window.location;
+    url = url.toString()
+    url = url.split("about.html");
+    url = url[0];
+   
+    var s1 = document.getElementById("starOne").src;
+    var s2 = document.getElementById("starTwo").src;
+    var s3 = document.getElementById("starThree").src;
+    var s4 = document.getElementById("starFour").src;
+    var s5 = document.getElementById("starFive").src;
+   
+   if (estrela == 5){ 
+    if (s5 == url + "star_empty.png") {
+    document.getElementById("starOne").src = "star_full.png";
+    document.getElementById("starTwo").src = "star_full.png";
+    document.getElementById("starThree").src = "star_full.png";
+    document.getElementById("starFour").src = "star_full.png";
+    document.getElementById("starFive").src = "star_full.png";
+    } else {
+    document.getElementById("starOne").src = "star_full.png";
+    document.getElementById("starTwo").src = "star_full.png";
+    document.getElementById("starThree").src = "star_full.png";
+    document.getElementById("starFour").src = "star_full.png";
+    document.getElementById("starFive").src = "star_empty.png";
+   }}
+    
+    //ESTRELA 4
+   if (estrela == 4){ 
+    if (s4 == url + "star_empty.png") {
+    document.getElementById("starOne").src = "star_full.png";
+    document.getElementById("starTwo").src = "star_full.png";
+    document.getElementById("starThree").src = "star_full.png";
+    document.getElementById("starFour").src = "star_full.png";
+    document.getElementById("starFive").src = "star_empty.png";
+    } else {
+    document.getElementById("starOne").src = "star_full.png";
+    document.getElementById("starTwo").src = "star_full.png";
+    document.getElementById("starThree").src = "star_full.png";
+    document.getElementById("starFour").src = "star_empty.png";
+    document.getElementById("starFive").src = "star_empty.png";
+   }}
+   
+   //ESTRELA 3
+   if (estrela == 3){ 
+    if (s3 == url + "star_empty.png") {
+    document.getElementById("starOne").src = "star_full.png";
+    document.getElementById("starTwo").src = "star_full.png";
+    document.getElementById("starThree").src = "star_full.png";
+    document.getElementById("starFour").src = "star_empty.png";
+    document.getElementById("starFive").src = "star_empty.png";
+    } else {
+    document.getElementById("starOne").src = "star_full.png";
+    document.getElementById("starTwo").src = "star_full.png";
+    document.getElementById("starThree").src = "star_empty.png";
+    document.getElementById("starFour").src = "star_empty.png";
+    document.getElementById("starFive").src = "star_empty.png";
+   }}
+    
+   //ESTRELA 2
+   if (estrela == 2){ 
+    if (s2 == url + "star_empty.png") {
+    document.getElementById("starOne").src = "star_full.png";
+    document.getElementById("starTwo").src = "star_full.png";
+    document.getElementById("starThree").src = "star_empty.png";
+    document.getElementById("starFour").src = "star_empty.png";
+    document.getElementById("starFive").src = "star_empty.png";
+    } else {
+    document.getElementById("starOne").src = "star_full.png";
+    document.getElementById("starTwo").src = "star_empty.png";
+    document.getElementById("starThree").src = "star_empty.png";
+    document.getElementById("starFour").src = "star_empty.png";
+    document.getElementById("starFive").src = "star_empty.png";
+   }}
+    
+    //ESTRELA 1
+   if (estrela == 1){ 
+    if (s1 == url + "star_empty.png") {
+    document.getElementById("starOne").src = "star_full.png";
+    document.getElementById("starTwo").src = "star_empty.png";
+    document.getElementById("starThree").src = "star_empty.png";
+    document.getElementById("starFour").src = "star_empty.png";
+    document.getElementById("starFive").src = "star_empty.png";
+    } else {
+    document.getElementById("starOne").src = "star_empty.png";
+    document.getElementById("starTwo").src = "star_empty.png";
+    document.getElementById("starThree").src = "star_empty.png";
+    document.getElementById("starFour").src = "star_empty.png";
+    document.getElementById("starFive").src = "star_empty.png";
+   }}
+    
+}
