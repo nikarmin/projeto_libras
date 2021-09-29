1751bd09726838378b21123ba115c9e944c97bf6
Nicoli <nikafworks@gmail.com>
2021-09-29 14:22:02 -0300

    Arrumando node

--- projeto-libras/html/index.js
+++ projeto-libras/html/index.js
@@ -1,5 +1,20 @@
 /*Arrumar o JavaScript*/ 
 
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req,res) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.end('ajuda deus');
+});
+
+server.listen(port,hostname, () =>{
+    console.log("servidor teorico rodando");
+})
+
+/*Login*/
 function btnLogin(a)
 {
     console.log(a);
