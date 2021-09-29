efb38a16e512b69109a33d0b500e283cb6c9d069
Nicoli <nikafworks@gmail.com>
2021-09-29 15:14:39 -0300

    Arrumando umas coisinhas

--- projeto-libras/html/server.js
+++ projeto-libras/html/server.js
@@ -1,7 +1,10 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
-const app = require('./app');
+const app = http.createServer((req, res) =>{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Olá mundo!\n');
+});
 
-const server = http.createServer(app);
-server.listen(port);
+app.listen(3000, '127.0.0.1');
+console.log('funcionando');
 
+//node server.js
