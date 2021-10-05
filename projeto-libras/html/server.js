d1831c2721f1d09b932f0b7c07672c8b3dbac45d
Nicoli <nikafworks@gmail.com>
2021-10-05 15:57:08 -0300

    Arrumando Server

--- projeto-libras/html/server.js
+++ projeto-libras/html/server.js
@@ -1,12 +1,3 @@
-/*const http = require('http');
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
-    res.end();
-});
-
-app.listen(3000, '127.0.0.1');*/
-
 //INICIANDO SERVER
 
 const express = require('express');
@@ -15,6 +6,7 @@ const routes = require('./routes');
 const app = express();
 const path = require('path');
 const sendMail = require('./email');
+const { route } = require('./routes');
 
 const PORT = 8080;
 
@@ -24,7 +16,6 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(express.static(__dirname + '/public'));
-//app.use(routes);
 
 app.post('/email', (req, res) => {
   //enviar email
@@ -43,9 +34,5 @@ app.post('/email', (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/about.html'));
 });
-
-/*app.get('/home.html', (req, res) => {
-  res.send(__dirname + '/home.html');
-});*/
 
 app.listen(PORT, () => console.log('Server está começando na porta, ', 8080));
