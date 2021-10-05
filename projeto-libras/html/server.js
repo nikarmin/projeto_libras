c2b1cd5d5a9e53530de775eb747820b5feea5fb5
Nicoli <nikafworks@gmail.com>
2021-10-05 11:49:34 -0300

    Adicionando Módulo IV

--- projeto-libras/html/server.js
+++ projeto-libras/html/server.js
@@ -3,15 +3,15 @@ http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(req.url);
     res.end();
-}).listen(8080);*/
+});
 
-//app.listen(3000, '127.0.0.1');
+app.listen(3000, '127.0.0.1');*/
 
 //INICIANDO SERVER
 
 const express = require('express');
+const routes = require('./routes');
 
-const log = console.log();
 const app = express();
 const path = require('path');
 const sendMail = require('./email');
@@ -23,8 +23,8 @@ const PORT = 8080;
 app.use(express.urlencoded({
   extended: false
 }));
-app.use(express.json());
-
+app.use(express.static(__dirname + '/public'));
+//app.use(routes);
 
 app.post('/email', (req, res) => {
   //enviar email
@@ -44,57 +44,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/about.html'));
 });
 
+/*app.get('/home.html', (req, res) => {
+  res.send(__dirname + '/home.html');
+});*/
+
 app.listen(PORT, () => console.log('Server está começando na porta, ', 8080));
-
-//adicionando endereço http com /moduloUm.html (exemplo)
-/*
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-http.createServer(function(req,res){
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data){
-        if(err){
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
-
-console.log('funcionando');
-*/
-//Enviar email
-/*
-require('dotenv').config();
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
-  }
-});
-
-let mailOptions = {
-  from: 'portalibras@gmail.com',
-  to: 'nikafworks@gmail.com',
-  subject: 'portal libras filho',
-  text: 'merda'
-}
-
-transporter.sendMail(mailOptions, function(err, info) {
-  if(err){
-    console.log("DEU RUIM"+err);
-  }
-  else{
-    console.log("DEU CERTO");
-  }
-})
-*/
-//node server.js
