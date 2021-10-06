7914b9085622c7bf006cd165d4c531607974ccf2
Nicoli <nikafworks@gmail.com>
2021-10-05 22:08:31 -0300

    Alterando API

--- projeto-libras/html/server.js
+++ projeto-libras/html/server.js
@@ -1,7 +1,25 @@
 //INICIANDO SERVER
 
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+});
+/*
 const express = require('express');
-const routes = require('./routes');
+const router = express.Router();
 
 const app = express();
 const path = require('path');
@@ -36,3 +54,4 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => console.log('Server está começando na porta, ', 8080));
+*/
