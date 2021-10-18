619dbea919986c47bce74ac6c28439544592daac
Nicoli <nikafworks@gmail.com>
2021-10-18 15:45:15 -0300

    Arrumando umas coisas

--- projeto-libras/html/routes.js
+++ projeto-libras/html/routes.js
@@ -5,18 +5,6 @@ routes.get('/', (req, res) => {
     return res.status(200).send({
         mensagem: 'beleza'
     });
-})
-
-routes.set('/', (req, res) => {
-    return res.json();
-})
-
-routes.patch('/', (req, res) => {
-    return res.json();
-})
-
-routes.delete('/', (req, res) => {
-    return res.json();
-})
+});
 
 module.exports = routes;
---
+++ projeto-libras/html/tabelaBD.sql
@@ -0,0 +1,75 @@
+
+create table Usuario(
+    idUsuario int primary key,
+    nick varchar(50)
+)
+
+create table Aulas(
+    idAula int primary key,
+    modulo varchar(3),
+    descricao varchar(15)   -- acho q pode até tirar dps
+)
+
+create table Conteudo(
+    idConteudo int primary key,
+    irlVideos varchar(2080),
+    irlImagem varchar(2080)
+)
+
+create table Conta(
+    idConta int primary key,
+    idUsuario int not null,
+    constraint idUsuario foreign key(idUsuario) references Usuario(idUsuario),
+    nome varchar(50) not null,
+    email varchar(256) not null,
+    cidade varchar(80),
+    pontosAcumulados int not null
+)
+
+create table Desempenho(
+    idDesempenho int primary key,
+    estrela int,
+    dataDesempenho data,
+    descricao varchar(100)
+)
+
+-- essa tabela não será usada
+create table Exercicios(
+    idExercicio int primary key,
+    alternativas varchar(5) not null,
+    pontos int not null,
+    tipo varchar(20),        
+    resposta varchar(1)
+)
+
+create table AulasConteudo(
+    idAulasConteudo int primary key,
+    idAula int not null,
+    constraint idAula foreign key(idAula) references Aulas(idUsidAulauario),
+    idConteudo int not null,
+    constraint idConteudo foreign key(idConteudo) references Conteudo(idConteudo)
+)
+
+create table ContaDesempenho(
+    idContaDesempenho int primary key,
+    idConta int not null,
+    constraint idConta foreign key(idConta) references Conta(idConta),
+    idDesempenho int not null,
+    constraint idDesempenho foreign key(idDesempenho) references Desempenho(idDesempenho)
+)
+
+create table AulasExercicios(
+    idAulasExercicios int primary key,
+    idAulasConteudo int not null,
+    constraint idAulasConteudo foreign key(idAulasConteudo) references AulasConteudo(idAulasConteudo),
+    idExercicio int not null,
+    constraint idExercicio foreign key(idExercicio) references Exercicios(idExercicio)
+)
+
+create table ContaExercicios(
+    idContaExercicios int primary key,
+    idConta int not null,
+    constraint idConta foreign key(idConta) references Conta(idConta),
+    idExercicio int not null,
+    constraint idExercicio foreign key(idExercicio) references Exercicios(idExercicio)
+)
