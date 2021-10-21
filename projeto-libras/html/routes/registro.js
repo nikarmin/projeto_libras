849f4013a7e58ddcc1bb83f4019b2558d76d4e7c
Nicoli <nikafworks@gmail.com>
2021-10-20 21:35:05 -0300

    Arrumando uma coisas

--- projeto-libras/html/routes/registro.js
+++ projeto-libras/html/routes/registro.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) => {
     res.status(200).send({
--- projeto-libras/html/server.js
+++ projeto-libras/html/server.js
@@ -1,37 +1,13 @@
 // API
 
-/*
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-const app = require('./app');
-const server = http.createServer(app);
-server.listen(3000);*/
-
-//INICIANDO SERVER
-/*
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);*/
-
 const express = require('express');
 const router = express.Router();
 const app = express();
 const path = require('path');
-const sendMail = require('./email');
+const sendMail = require('../html/email');
 const { route } = require('./routes');
 
-const PORT = 8080;
+const PORT = 3000;
 
 //DATA PARSING
 
@@ -98,4 +74,9 @@ app.get('/cadastro.html', (req, res) => {
   res.sendFile(path.join(__dirname + '/cadastro.html'));
 });
 
-app.listen(PORT, () => console.log('Server está começando na porta, ', 8080));
+// fazer carregar css
+
+app.use('/css', express.static('public'))
+app.use('/modulo.css', express.static(__dirname + '/css'));
+
+app.listen(PORT, () => console.log('Server está começando na porta, ', 3000));
